791968798965a29a113eeab780191aad1c2f9769
Brett Commandeur <bcmdr.co@gmail.com>
2017-12-06 13:57:33 -0700

    progress on component refactor, learn you can't nest vms

--- public/scripts/AppVM.js
+++ public/scripts/AppVM.js
@@ -1,17 +1,59 @@
 class AppVM {
   constructor() {
-    let date = new Date()
-    this.vm = new Vue({
+    Vue.component('tree', {
+      template: `
+        <div id="tree-{{id}}" class="tree-card-wide mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">{{title}}</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <p>{{description}}</p>
+            <button v-on:click="$emit('remove')">X</button>\
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              Done
+            </a>
+          </div>
+          <div class="mdl-card__menu">
+            <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+              <i class="material-icons">info</i>
+            </button>
+          </div>
+        </div>
+      `,
+      props: [
+        'id', 'title', 'description'
+      ]
+    })
+
+    new Vue({
       // element to mount to
       el: '#app',
       // initial data
       data: {
         isSignedIn: false,
         signInStatusKnown: false,
-        date: date,
-        tree: {
-          title: "Work On Orhcard"
-        },
+        date: new Date(),
+        newTreeText: '',
+        trees: [
+          {
+            id: 1,
+            title: 'Work On Orchard',
+            description: '3 Git Pushes per Day'
+          },
+          {
+            id: 2,
+            title: 'Stay In Shape',
+            description: '10 Push Ups per Day'
+          },
+          {
+            id: 3,
+            title: 'Stay Hydrated',
+            description: '3 Waterbottle refills per day'
+          }
+        ],
+        nextTodoId: 4,
       },
       // firebase binding
       // https://github.com/vuejs/vuefire
@@ -20,7 +62,6 @@ class AppVM {
       },
       // methods
       methods: {
-
         signinGoogle: function () {
           // https://firebase.google.com/docs/auth/web/google-signin
           var provider = new firebase.auth.GoogleAuthProvider();
@@ -36,7 +77,16 @@ class AppVM {
             // An error happened.
             console.log(error)
           });
+        },
+
+        addNewTree: function () {
+          this.trees.push({
+            id: this.nextTreeId++,
+            title: this.newTreeText
+          })
+          this.newTreeText = ''
         }
+        
       },
       computed: {
         today: function() {
--- public/scripts/TreeVM.js
+++ public/scripts/TreeVM.js
@@ -1,6 +1,6 @@
-class TreesVM {
+class TreeVM {
   constructor() {
-    Vue.component('user-tree', {
+    Vue.component('tree', {
       template: `
         <div id="tree-{{id}}" class="tree-card-wide mdl-card mdl-shadow--2dp">
           <div class="mdl-card__title">
@@ -8,6 +8,7 @@ class TreesVM {
           </div>
           <div class="mdl-card__supporting-text">
             <p>{{description}}</p>
+            <button v-on:click="$emit('remove')">X</button>\
           </div>
           <div class="mdl-card__actions mdl-card--border">
             <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
@@ -26,24 +27,34 @@ class TreesVM {
       ]
     })
 
-    this.vm = new Vue({
-      #el: '#user-trees',
+    new Vue({
+      el: '#user-trees',
       data: {
         newTreeText: '',
         trees: [
           {
-            id: 1
+            id: 1,
             title: 'Work On Orchard',
-            description: 'Write code every day and submit at least 3 pushes to the github repo'
+            description: '3 Git Pushes per Day'
+          },
+          {
+            id: 2,
+            title: 'Stay In Shape',
+            description: '10 Push Ups per Day'
+          },
+          {
+            id: 3,
+            title: 'Stay Hydrated',
+            description: '3 Waterbottle refills per day'
           }
         ],
-        nextTreeId: 2
+        nextTodoId: 4
       },
       methods: {
         addNewTree: function () {
           this.trees.push({
             id: this.nextTreeId++,
-            title: this.newTreeText,
+            title: this.newTreeText
           })
           this.newTreeText = ''
         }
